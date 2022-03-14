692f28e1ed7db0d0de1f4535a33f2b2cec1381dd
ADMIN\NagaSai <nagasaibeeraka26@gmail.com>
2022-03-15 00:00:46 +0530

    ts commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>
+          Learn React - Typescript
+        </h1>
       </header>
     </div>
   );
